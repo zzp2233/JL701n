716859419af985dc960a21368af2aed3fde08b7d
zzp2233 <64593779+zzp2233@users.noreply.github.com>
2025-07-11 11:42:42 +0800

    Create le_smartbox_adv.h

---
+++ le_smartbox_adv.h
@@ -0,0 +1,24 @@
+// binary representation
+// attribute size in bytes (16), flags(16), handle (16), uuid (16/128), value(...)
+
+#ifndef _LE_SMARTBOX_ADV_H
+#define _LE_SMARTBOX_ADV_H
+
+#include <stdint.h>
+#include "bt_common.h"
+#if (TCFG_BLE_DEMO_SELECT == DEF_BLE_DEMO_RCSP_DEMO)
+
+u8 get_adv_notify_state(void);
+void set_adv_notify_state(u8 notify);
+int smartbox_make_set_rsp_data(void);
+int smartbox_make_set_adv_data(void);
+void bt_ble_init_do(void);
+int bt_ble_adv_ioctl(u32 cmd, u32 priv, u8 mode);
+void set_connect_flag(u8 value);
+void upay_ble_mode_enable(u8 enable);
+int upay_ble_adv_data_set(void);
+int upay_ble_send_data(const uint8_t *data, u16 len);
+void upay_ble_regiest_recv_handle(void (*handle)(const uint8_t *data, u16 len));
+
+#endif
+#endif
